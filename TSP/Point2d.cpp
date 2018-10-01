670ba38cf3f87b2d5c42d1498b92ecd94ac2b882
Andy Wang <jlin3182@sina.com>
2018-10-01 20:36:10 +0800

    增加了图和点的定义，忽略了tsp文件

--- TSP/Point2d.cpp
+++ TSP/Point2d.cpp
@@ -2,11 +2,15 @@
 #include "Point2d.h"
 
 
-CPoint2d::CPoint2d()
+double CPoint2d::DistanceTo(const CPoint2d& pt)
 {
+	double xx = (x - pt.x)*(x - pt.x);
+	double yy = (y - pt.y)*(y - pt.y);
+	return sqrt(xx + yy);
 }
-
-
-CPoint2d::~CPoint2d()
+CPoint CPoint2d::ToCPoint()
 {
+	return CPoint(static_cast<int>(x*m_scale), static_cast<int>(y*m_scale));
 }
+size_t CPoint2d::all_id = 0;
+double CPoint2d::m_scale = 1.0;
--- TSP/Point2d.h
+++ TSP/Point2d.h
@@ -1,17 +1,32 @@
 #pragma once
 #include "afx.h"
-class CPoint2d :
+#include "Graph.h"
+
+class CGraph;
+class CPoint2d:
 	public CObject
 {
 public:
-	CPoint2d(double x, double y) :m_scale(1.0) { this->x = x; this->y = y; }
-	CPoint2d() :x(0.0), y(0.0), m_scale(1.0) {}
-	~CPoint2d();
-	double DistTo(CPoint2d);
-	void SetScale(double scale) { m_scale = scale; }
+	friend class CGraph;
+public:
+	CPoint2d(double x, double y) :this_id(all_id++) { this->x = x; this->y = y; visited = false; }
+	CPoint2d() :x(0.0), y(0.0), this_id(all_id++), visited(false) {}
+	double DistanceTo(const CPoint2d&);
+	inline void SetScale(double scale) { m_scale = scale; }
+	CPoint2d(const CPoint2d& pt) :x(pt.x), y(pt.y), this_id(pt.this_id), visited(pt.visited) {}
+	CPoint2d& operator=(CPoint2d pt)
+	{
+		return CPoint2d(pt);
+	}
+	CPoint ToCPoint();
 	double x;
 	double y;
+	size_t GetID() { return this_id; }
 private:
-	double m_scale;
+	static double m_scale;
+	static size_t all_id;
+	size_t this_id;
+	bool visited;
 };
+
 
